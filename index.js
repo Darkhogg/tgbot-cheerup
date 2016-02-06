7ad8d0ba26841ce4283e6cd1bac0a5f159693e59
Daniel Escoz <darkhogg@gmail.com>
2016-02-06 13:14:01 +0100

    Add crash log

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 const crashit = require('crashit');
+
+const log = require('./log');
 const Bot = require('./bot');
 
-crashit.handleSignals(['SIGINT', 'SIGTERM'])
-crashit.handleUncaught();
+crashit.addHook((cause) => {
+    log.warn('[App]  shutting down: %s', cause);
+});
+
+crashit.handleSignals(['SIGINT', 'SIGTERM', 'SIGUSR2'], true)
+crashit.handleUncaught(true);
 process.on('unhandledRejection', (err) => {
     console.error(err.stack);
-    crashit.crash(err);
+    crashit.crash(err, true);
 });
 
 Bot().then(bot => {
